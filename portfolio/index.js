eb31e3c3e1e64eaca403cd8e5befdd91f31b71f0
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-16 15:23:16 -0500

    redid 404 page, changed index to home

---
+++ portfolio/index.js
@@ -0,0 +1,5 @@
+// document.getElementById("home").addEventListener("click", function (e) {
+//   e.preventDefault();
+//   window.location.href = "https://jacobburnstein.com/home";
+//   console.log("testing");
+// });
