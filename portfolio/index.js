f56eea80d338f95ed31381db9927b50d3aa12f3a
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-16 21:10:37 -0500

    hash routing set up

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,5 +1,29 @@
-// document.getElementById("home").addEventListener("click", function (e) {
-//   e.preventDefault();
-//   window.location.href = "https://jacobburnstein.com/home";
-//   console.log("testing");
-// });
+function render() {
+  getHash();
+  showContent();
+}
+
+function getHash() {
+  const hash = window.location.hash.slice(1);
+  return hash;
+}
+
+function showContent() {
+  const mainContent = document.getElementById("main-content");
+
+  let hash = getHash();
+  console.log(hash);
+  console.log("hash-content: ", `${hash}-content`);
+  const contentToShow = document.querySelector(`.${hash}-content`);
+  console.log("content to show: ", contentToShow);
+
+  mainContent.innerHTML = contentToShow
+    ? contentToShow.innerHTML
+    : "content not found";
+
+  // mainContent.replaceChildren = contentToShow;
+}
+
+window.addEventListener("hashchange", showContent);
+
+render();
