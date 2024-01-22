ea60fb2dc4982eba76fda4f8ccde79f7560d5c17
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-01-22 10:59:33 -0500

    deleted unused files, decreased top padding for about and project views

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,24 +1,3 @@
-// function getHash() {
-//   const hash = window.location.hash.slice(1);
-//   return hash;
-// }
-
-// function goHome() {
-//   let hash = getHash();
-//   if (hash) {
-//     // console.log(hash);
-//     // console.log("hash-content: ", `${hash}-content`);
-//     contentToShow = document.getElementById(`${hash}-content`);
-//     console.log("content to show: ", contentToShow);
-//   }
-
-//   mainContent.innerHTML = contentToShow
-//     ? contentToShow.outerHTML
-//     : "content not found";
-
-//   // mainContent.replaceChildren = contentToShow;
-// }
-
-// window.addEventListener("hashchange", goHome);
-
-// goHome();
+console.log(
+  "If you're reading this, then you already know...I'm Jacob Burnstein. Thanks for visiting my site!"
+);
