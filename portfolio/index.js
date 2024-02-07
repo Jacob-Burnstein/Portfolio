031dda5ab4faffb3f8ec47e0888b230996bc019f
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-07 18:47:47 -0500

    basic function and icon changing for toggle dark mode

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,32 @@
-console.log(
-  "If you're reading this, then you already know...I'm Jacob Burnstein. Thanks for visiting my site!"
-);
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+const enableDarkMode = () => {
+  document.body.classList.add("darkMode");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("darkMode");
+};
+
+const toggleDarkMode = () => {
+  if (document.body.classList.contains("darkMode")) {
+    disableDarkMode();
+    darkModeToggle.classList.remove("fa-solid", "fa-sun");
+    darkModeToggle.classList.add("fa-regular", "fa-moon");
+  } else {
+    enableDarkMode();
+    darkModeToggle.classList.remove("fa-regular", "fa-moon");
+    darkModeToggle.classList.add("fa-solid", "fa-sun");
+  }
+};
+
+darkModeToggle.addEventListener("click", toggleDarkMode);
+
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+const matchUserSettings = () => {
+  if (prefersDarkMode.matches) {
+    enableDarkMode();
+  }
+};
+
+matchUserSettings();
