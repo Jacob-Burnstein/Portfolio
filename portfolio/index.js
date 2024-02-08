c67b3b59ff1c123b7de986810fc28a26eab1f032
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-08 08:56:05 -0500

    temporary fix for mobile nav bar

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,15 +1,28 @@
 const darkModeToggle = document.querySelector(".dark-mode-toggle");
 
+const nav = document.querySelector(".nav-bar");
+
+const addDarkHeaderStyling = () => {
+  nav.classList.add("dark-mode-header");
+};
+
+const removeDarkHeaderStyling = () => {
+  if (nav.classList.contains("dark-mode-header"))
+    nav.classList.remove("dark-mode-header");
+};
+
 const enableDarkMode = () => {
   document.body.classList.add("darkMode");
   darkModeToggle.classList.remove("fa-regular", "fa-moon");
   darkModeToggle.classList.add("fa-solid", "fa-sun");
+  addDarkHeaderStyling();
 };
 
 const disableDarkMode = () => {
   document.body.classList.remove("darkMode");
   darkModeToggle.classList.remove("fa-solid", "fa-sun");
   darkModeToggle.classList.add("fa-regular", "fa-moon");
+  removeDarkHeaderStyling();
 };
 
 const toggleDarkMode = () => {
